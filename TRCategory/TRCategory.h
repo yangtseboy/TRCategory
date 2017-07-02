90ddb98602fe88146174d619ca7c0d9d38822f6f
xuyang <xuyang@xuyangdemacbook-pro-2.local>
2017-07-02 16:39:43 +0800

    Initial Commit

---
+++ TRCategory/TRCategory.h
@@ -0,0 +1,19 @@
+//
+//  TRCategory.h
+//  TRCategory
+//
+//  Created by xuyang on 2017/7/2.
+//  Copyright © 2017年 travel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TRCategory.
+FOUNDATION_EXPORT double TRCategoryVersionNumber;
+
+//! Project version string for TRCategory.
+FOUNDATION_EXPORT const unsigned char TRCategoryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TRCategory/PublicHeader.h>
+
+
